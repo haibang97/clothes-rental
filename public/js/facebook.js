2673f87efe109c3aec6af8467efca08fd3a55b63
LZL96 <zuolin.liu.2017@sis.smu.edu.sg>
2019-03-14 20:22:27 +0800

    facebook test

--- public/js/facebook.js
+++ public/js/facebook.js
@@ -16,17 +16,17 @@ function test() {
 }
 
 function fbLogin() {
-    // FB.login(function(response) {
-    //     if (response.authResponse) {
-    //     console.log('Welcome!  Fetching your information.... ');
-    //     FB.api('/me', function(response) {
-    //     console.log('Good to see you, ' + response.name + '.');
-    //     });
-    //     } else {
-    //     console.log('User cancelled login or did not fully authorize.');
-    //     }
-    // }, {scope: "email,first_name,last_name,gender,birthday",
-    //     return_scopes: true});
+    FB.login(function(response) {
+        if (response.authResponse) {
+        console.log('Welcome!  Fetching your information.... ');
+        FB.api('/me', function(response) {
+        console.log('Good to see you, ' + response.name + '.');
+        });
+        } else {
+        console.log('User cancelled login or did not fully authorize.');
+        }
+    }, {scope: "email,first_name,last_name,gender,birthday",
+        return_scopes: true});
     fillForm();
 }
 
