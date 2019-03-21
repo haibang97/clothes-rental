94f47fa5e8b37a1dc4649efaf28240b172f11d30
LZL96 <zuolin.liu.2017@sis.smu.edu.sg>
2019-03-21 18:00:52 +0800

    ffb

--- public/js/facebook.js
+++ public/js/facebook.js
@@ -36,7 +36,7 @@ function sleep(miliseconds) {
 
 function getLoginStatus() {
     // alert("get login status called");
-    logout();
+    // logout();
     FB.getLoginStatus(function(response) {
         if (response.status === 'connected') {
             var uid = response.authResponse.userID;
