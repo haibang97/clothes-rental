ae315c30499cd0c5e96ca25cdd51e4b8684d07f7
Xavier Lur <xavier.lur.2017@sis.smu.edu.sg>
2019-03-31 03:05:46 +0800

    update server.js

--- server.js
+++ server.js
@@ -34,12 +34,12 @@ app.get('/clothes-details', async function (req, res) {
   var clothesid = req.query.clothesid
 
   // var host = 'http://LAPTOP-M5IE8VM3:8081'
-  var host = 'http://10.124.1.237:8081'
-  // var host = "https://liuzuolin1996-eval-test.apigee.net/"
-  // var service = "clothes"
+  // var host = 'http://10.124.1.237:8081'
+  var host = "https://liuzuolin1996-eval-test.apigee.net/"
+  var service = "clothes"
   var body = '/Clothes/getSpecificClothes/'
-  // var url = host + service + body + clothesid
-  var url = host + body + clothesid
+  var url = host + service + body + clothesid
+  // var url = host + body + clothesid
 
   var clothesDetails = await getClothesDetails(url)
   console.log(typeof clothesDetails)
