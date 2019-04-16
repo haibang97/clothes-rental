8a9b270ca840124c6aef65685435334bd193e82d
LZL96 <zuolin.liu.2017@sis.smu.edu.sg>
2019-04-16 17:37:57 +0800

    edited flask services

---
+++ flask_services/main.py
@@ -0,0 +1,42 @@
+from flask import Flask, jsonify, request
+from services import * 
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return jsonify([
+        {"READ":"ME"},
+        {"message":"web services in python flask added (in progress)"},
+        {"secondary message":"this is so much better than tibco"},
+        {"tertiary message":"so far we have /get_backgrounds, /get_all_customers, /get_all_clothes, /get_all_clothesclasses, /get_all_deliveryman"}
+    ])
+
+@app.route("/get_backgrounds")
+def get_backgrounds_():
+    return jsonify(get_backgrounds())
+
+@app.route("/get_all_customers")
+def get_all_customers_():
+    return jsonify(get_all_customers())
+
+@app.route("/get_all_clothes")
+def get_all_clothes_():
+    return jsonify(get_all_clothes())
+
+@app.route("/get_all_clothesclasses")
+def get_all_clothesclasses_():
+    return jsonify(get_all_clothesclasses())
+
+@app.route("/get_all_deliveryman")
+def get_all_deliveryman_():
+    return jsonify(get_all_deliveryman())
+
+
+# @app.route("/add", methods=["GET"])
+# def add():
+#     name = request.args.get("name")
+#     age = int(request.args.get("age"))
+#     breed = request.args.get("breed")
+#     return jsonify(addDog(name,age,breed))
+
+app.run(debug=True)
---
+++ flask_services/test.sql
@@ -0,0 +1,20 @@
+-- use background;
+-- select * from background;
+
+-- use clothesclass;
+-- select * from clothes;
+-- select * from clothesclass;
+
+-- use customers;
+-- select * from customers;
+
+use delivery;
+-- select * from deliveryman;
+select * from delivery_schedule;
+
+-- use orders;
+-- select * from orders;
+
+-- use database test;
+
+
